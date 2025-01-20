704f6b86814fc7bc6aa05f3f6c198ecb9245d25b
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 12:40:15 -0600

    delete

--- 01_hello_world.py
+++
@@ -1,7 +0,0 @@
-#Asks user what is your name?
-name = input("What is your name?") 
-#Remove blank whitespace from str & Capitalize users name (chain up)
-name = name.strip().capitalize()
-# f gives a hint to python to treat the function specially.
-print(f"Hello, {name}")
-
