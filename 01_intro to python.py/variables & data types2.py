a04950dcb7f182aa9f880fa642094f0601cf5d6d
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 11:51:39 -0600

    Notes for variables and data types

---
+++ 01_intro to python.py/variables & data types2.py
@@ -0,0 +1,4 @@
+Name = input("Name:") # Quotes are used for string remaining constant
+Age = int(input("Age:")) #int
+Weight = float(input("Weight:"))
+print = ("My name is", Name , "and my age is", Age , "Also, my weight is" , Weight ,"Kgs")
