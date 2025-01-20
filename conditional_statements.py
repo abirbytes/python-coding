b92ca174eedbb8a4fe63aff7f67ac09c976b4b41
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 13:25:52 -0600

    Traffic lights and grading system code

--- conditional_statements.py
+++ conditional_statements.py
@@ -9,13 +9,13 @@ else(condition): [used when elif and if not working]
 Use 4 gaps for before statement as it comes in if known as indentation'''
 #Rules of python are known as syntax
                 #Example Code: Traffic Lights
-'''light= input("The color of light is :")
+light= input("The color of light is :")
 if(light=="Red"):
    print("Stop")
 elif(light=="Green"):
    print("Go")
 else:
-   print("Light is not working")'''
+   print("Light is not working")
               #Example code : Grades for 10th graders....
 Marks = input("Obtained marks in the exam: ")
 if(Marks>="95"):
