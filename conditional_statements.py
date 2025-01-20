59f7d077c442ff414ab3ee39601aa644e69ccfd6
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 12:47:31 -0600

    Summary

---
+++ conditional_statements.py
@@ -0,0 +1,9 @@
+#statments in which we have a condition are known as conditional statements
+'''Key words for conditional statements in python are if-elif-else
+if(condition):
+       Statement1
+elif(condition):
+      Statement2
+else(condition):
+      StatementN'''
+
