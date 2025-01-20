0d39f6470f0ae9567fa10a9d2db6f6358e0dd499
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 12:34:50 -0600

    How to input in python.

--- 01_variables & data types.py/Input.py
+++ 01_variables & data types.py/Input.py
@@ -4,4 +4,8 @@ Weight = float(input("Weight: "))  # Convert input to a float
 
 # Corrected print statement and never put = after print
 print("My name is", Name, "and my age is", Age, ". Also, my weight is", Weight, "Kgs")
+#not>and>or
+'''expression = not true and false or true
+according to operating precedance,, not true = false
+therefore, expression = False and False or true and the "and" operator says if any of the value is false then the whole thing will be false... Therefore, expression = False or True, and the 'or" operator says if any value is true then the whole expression will be true. Thus, the output if the expression is evaluated will be True.'''
 
