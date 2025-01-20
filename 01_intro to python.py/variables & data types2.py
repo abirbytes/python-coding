1dcf6e77aa8cd769f6bf04481e8272dc426f70b0
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 11:52:52 -0600

    xxx

--- 01_intro to python.py/variables & data types2.py
+++
@@ -1,4 +0,0 @@
-Name = input("Name:") # Quotes are used for string remaining constant
-Age = int(input("Age:")) #int
-Weight = float(input("Weight:"))
-print = ("My name is", Name , "and my age is", Age , "Also, my weight is" , Weight ,"Kgs")
