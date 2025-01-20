646cf11f7c378827716e3b7173794f8b235ba73d
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-20 12:02:46 -0600

    How to do input in python?

---
+++ 01_variables & data types.py/Input.py
@@ -0,0 +1,7 @@
+Name = input("Name: ")  # Get the name as a string
+Age = float(input("Age: "))  # Convert input to an integer
+Weight = float(input("Weight: "))  # Convert input to a float
+
+# Corrected print statement and never put = after print
+print("My name is", Name, "and my age is", Age, ". Also, my weight is", Weight, "Kgs")
+
--- 01_variables & data types.py/var and data types P-1.py
+++ 01_variables & data types.py/var and data types P-1.py
@@ -54,5 +54,5 @@ print(F)# Remainder or % modulo is negative only when denominatore is negative..
 Name = input("Name:") # Quotes are used for string remaining constant
 Age = int(input("Age:")) #int
 Weight = float(input("Weight:"))
-print = ("My name is", Name , "and my age is", Age , "Also, my weight is" , Weight ,"Kgs")
+print("My name is", Name, "and my age is", Age, "Also, my weight is" , Weight ,"Kgs")
 
