6df339ee5e58f91828c13ebb44f7793bc6aaeaea
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-23 21:43:22 -0600

    Intro to strings

---
+++ 14_intro_to_strings_Ch-3.py
@@ -0,0 +1,7 @@
+name= "Abir" # [A=0 or -4 and r = 3 or -1]
+nameshort= name[0:3] #Start from index 0 to all the way till 3 (excluding 3)
+#Last index is never included
+print(nameshort)
+
+charachter= name[3]#stores the charachter inidicated in the bracket by zero to the variable
+print(charachter)
---
+++ 15_negative_slicing.py
@@ -0,0 +1,12 @@
+name = "Abir"
+print(name[0:3])
+print(name[-4:-3])
+print(name[0:1])
+# basically in Abir 
+# A=0 or -4
+# b=1 or -3
+# i=2 or -2
+# r=3 or -1
+print(name[:4]) #if there is nothing in the upper index then we can assume 0....
+print(name[0:]) # Equal to [0:4]
+print(name[0:4])
