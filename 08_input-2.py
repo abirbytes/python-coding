4dfa15d8f35d298d64f8af7b9366a2df7c7ffbbe
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-23 18:28:16 -0600

    Summary of input function in python

---
+++ 08_input-2.py
@@ -0,0 +1,5 @@
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("Number a is: ", a) # Shift + Alt + Down to copy the same....
+print("Number b is: ", b)
+print("Sum is: " , a+b) #Ans will be 23 if a = 2 and b=3 cause python takes input in str form and if we add "Happy" + "world" = HappyWorld,,,so we must change the input to integer to get proper value when we want any arithmetic operation to be performed
