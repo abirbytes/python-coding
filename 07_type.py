cf8985b10b9bb072c0ac61c1594054d9f80ec626
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-23 18:14:07 -0600

    Changing type as you changed yours after your ex

---
+++ 07_type.py
@@ -0,0 +1,8 @@
+a= "31.2" #anything inside the double quote is a string
+b= float(a) # a but change the type from string to float
+t = type(b) # if we put a we will get str but when we put b we will get float
+print(t)
+d= float(31) # Integer to Float
+print(d)
+e= int(38.38) # Float to integer..
+print(e)
