7b7057db07f624159b81c6990c159ddae6f5cb8f
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-23 18:03:49 -0600

    All types of operators in python.

---
+++ 06_operators.py
@@ -0,0 +1,24 @@
+# Arithmetic Operators
+a=7
+b=4
+c=a+b
+print(c)
+
+#Assingment Operators
+a = 4-2 # Assing 4-2 in a
+print(a)
+b = 6
+# b +=3 #Increment the value of b by 3 and then assign it to  b
+b -=3 #Decrement the value of b by 3 and then assign it to b
+print(b)
+
+#Comparison Operators
+# d = 5>4
+d= 5==4 # use double equal to chck if numbers are equal to each other and to assign value in a variable use one equal sign.....
+print(d)
+#Logical Operators
+e= True or False# In case of or oif one of them is true it will return true, e.g. Human or Fish , True cause you might be human but if it is a fish or fish its false cause you can't be fish [One atleast true]
+print (e)
+f = True and False # It cant be true and false together so there should be True and true to get an output of True otherwise it is false [Both true required]
+print(f)
+print(not(True)) # Turns the true to false and false to true,, basically reversing the thing....
