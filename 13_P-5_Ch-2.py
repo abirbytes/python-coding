c8c12e8bd7571b59b7d36183887b39f664d44190
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-23 18:55:45 -0600

    Solution

---
+++ 13_P-5_Ch-2.py
@@ -0,0 +1,3 @@
+#to calculate the square of a number entered by the user
+a= int(input("Give me the square of "))
+print("The square of",a,"is", a**2) #Use ** to make a number the exponent of the other,,makes it more easy and efficient
