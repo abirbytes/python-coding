690c0ab24c7c540ad3a96876c119e901924bca0c
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-24 18:42:05 -0600

    skipping value

---
+++ 16_slicing with skip value.py
@@ -0,0 +1,5 @@
+word = "amazing"
+print(word[1:6:2]) # Here first we have to find the value of 1:6 which will be 'mazin' and then for 2 we will jump over two letters until there is no letter to jump over.... so the answer will be mzn......... 
+#Always write the first letter after resolving
+print(word[:7]) # word [0:7] - 'amazing'
+print(word[0:]) #word [0:7] 'amazing' and the length is the lower index
