7144e53444e324160a20c0d90d053a7acd8b6488
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-24 19:53:23 -0600

    Problem 1 Chapter 3

---
+++ 19_Ch-3_P-1.py
@@ -0,0 +1,5 @@
+#Write a python program to display a user entered name followed by Good Afternoon using input() funtion
+name=input("Username:")
+print("Good Afternoon",name)
+#OR
+print(f"Good Afternoon {name}")#After marking the whole string with f function before the string to mark it as f string we can write the variable inside the string using curly braces
