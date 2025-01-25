2cd2d8c5edb9bd9fd9a626027949b2a6504866eb
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-24 19:42:23 -0600

    Escape Sequences

---
+++ 18_escape_seq.py
@@ -0,0 +1,9 @@
+#How to create multiple lines in single and double quotes strings
+a="New York is one of the best cities in the world\nwith it's best skyline"
+print(a) 
+print("She is very pretty\nHer name is Aysha") 
+#Can use \n to create two or multiple lines
+#Imp>>>>
+print("She is very pretty\nHer name is \"Aysha\"") #\"Aysha\" can be used to print a letter of a string in double quotes 
+#Exceptions>>>>>>>>>>
+print("She is very pretty\nHer name is 'Aysha'") #'Aysha' can be used to print a letter of a string in single quotes quotes 
