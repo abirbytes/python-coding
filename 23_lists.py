916b669c901f80b13d0849cb95cbbec19a365f45
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-24 20:46:47 -0600

    lists

---
+++ 23_lists.py
@@ -0,0 +1,5 @@
+lists=["Abir","Samonnoy",69,78.88,True,"Lamia"]
+print(lists[0])
+lists[0]="Rhythm"#unlike strings lists are mutable and can change it not being constant
+print(lists[0:4])
+print(lists[0:3:2])
