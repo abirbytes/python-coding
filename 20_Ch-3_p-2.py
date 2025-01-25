e88dcff93a1f1d0f18462463b31e71f4d3471261
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-24 20:05:57 -0600

    .replace

---
+++ 20_Ch-3_p-2.py
@@ -0,0 +1,4 @@
+letter = '''Dear Name
+           You are selected!
+           Date'''
+print(letter.replace("Name",input("My name is:")).replace("Date",input("Today's Date is:"))) #By using replace we can make multiple changes in a string together
