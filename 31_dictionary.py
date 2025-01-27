ff594722b692828984b41faccc5fdfd7ae80d2a2
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-26 19:58:14 -0600

    properties of dictionaries in python

---
+++ 31_dictionary.py
@@ -0,0 +1,12 @@
+# Properties of python dictionaries:
+# # 1.It is unordered
+# # 2.It is mutable
+# # 3.It is indexed
+# # 4.Cannot contain duplicate keys
+marks={
+   "Abir": 100,
+   "Lamia" : 90,
+   "Tam":80
+}
+print(marks,type(marks))
+print(marks["Lamia"])#prints the value of the key corresponding to it
