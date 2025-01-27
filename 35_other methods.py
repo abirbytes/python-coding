6342ff66f9391c15339172550956ea540d4f2125
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-26 21:05:04 -0600

    Essential Methods for sets

---
+++ 35_other methods.py
@@ -0,0 +1,18 @@
+s1={1,5,32,54,5,5,"Abir"}
+s2={2,5,32,84,5,7,"Kabir"}
+#print(s.union(s2))
+# >>>
+# #Gives a set with all the elements of the sets without repeating 
+
+print(s1.intersection(s2))#Makes a set with only commpn values in both sets
+
+set3 = {1, 2, 3}
+set4 = {3, 4, 5}
+result = set3.difference(set4)  # or set3 - set4
+print(result)  # Output: {1, 2}Returns a set with elements that are in the first set but not the second.
+set5 = {1, 2}
+set6 = {1, 2, 3}
+result = set6.issuperset(set5)
+print(result)  # Output: True
+result2 = set5.issubset(set6)
+print(result2)
