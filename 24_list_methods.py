2eaee25da7ed30664b1a5ca1a37153a2eab6aff6
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-26 18:48:59 -0600

    lists

---
+++ 24_list_methods.py
@@ -0,0 +1,20 @@
+#Rule No.1>>>
+l=["Abir","Samonnoy","Lamia","Debadrita"]
+l.append("Sahara")# Adds sahara at the end of the list
+print(l)
+#Rule No.2>>>
+l.reverse()#To write the list reversely
+print(l)
+#Rule No.3>>>
+l.sort() # To sort orderly
+print(l)
+#Rule No.4>>>
+l.insert(3,8)# Will add 8 in the index 3 for example in (x,y) x is the index of list and y is the thing that will be inserted in that index
+print(l)
+#Rule No.5>>>
+value = l.pop(2)
+print(value)#Prints the value that is removed
+print(l.pop(2))
+
+
+
