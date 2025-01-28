685976abb84d256ebc1234168fc44ef05e14a7a3
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-01-27 19:35:13 -0600

    solution

---
+++ 41_Ch-5_P-6.py
@@ -0,0 +1,13 @@
+d={
+}
+n1=input("My name is: ")
+v1=input("My language is:")
+n2=input("My name is: ")
+v2=input("My language is:")
+n3=input("My name is: ")
+v3=input("My language is:")
+n4=input("My name is: ")
+v4=input("My language is:")
+d.update({n1:v1,n2:v2,n3:v3,n4:v4})
+print(d)
+#In dictionary values can be same for two different keys but keys cant be the same for different values since its the identifier
