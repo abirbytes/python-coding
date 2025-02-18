ea205f8c448f489a694022d4976c46fca1f038fe
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-17 19:03:02 -0600

    Chapter 6 Problem 1

---
+++ 43_Ch-6_P-1.py
@@ -0,0 +1,14 @@
+n1 = int(input("The number is:"))
+n2 = int(input("The number is:"))
+n3 = int(input("The number is:"))
+n4 = int(input("The number is:"))
+
+if(n1>n2 and n1>n3 and n1>n4):
+   print("Greatest number is:",n1)
+elif(n2>n1 and n2>n3 and n2>n4):
+   print("Greatest number is:",n2)
+elif(n3>n1 and n3>n2 and n3>n3):
+   print("Greatest number is:",n3)
+elif(n4>n1 and n4>n2 and n4>n3):
+   print("Greatest number is:",n4)
+
