34d8f71a86fb7ae6d094ae11d81e94b5c69bb2d4
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-17 21:18:54 -0600

    p-3

---
+++ 56_ch-7_p-3.py
@@ -0,0 +1,14 @@
+#Method 1
+# num=int(input("Enter the number:"))
+# i=0
+# while(i<=10*num):
+#    print(i)
+#    i += num
+
+#Method 2
+
+n = int(input("enter a number:"))
+i = 0
+while(i<=10):
+   print(f"{n} x {i} = {n * i}")
+   i+=1
