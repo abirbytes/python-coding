cd2a7bd14135d035a8b3da5fee19920a437fe4a7
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-17 20:20:16 -0600

    while loops

---
+++ 50_whileloops.py
@@ -0,0 +1,11 @@
+i=1
+while(i<=50): #The block keeps executing until the condition is true.
+   print(i)
+   i +=1
+#In while loops, the condition is checked first. If it evaluates to true, the body of the loop is executed, otherwise not!
+
+#If the loop is entered, the process of [condition check and execution] is continued untikl the condition becomes false.
+i2= 1
+while(i2<=5):
+   print("Abir")
+   i2=i2+1
