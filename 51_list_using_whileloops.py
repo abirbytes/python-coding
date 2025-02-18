3c6d7b133c390fe22722d6d0b739127b6735f253
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-17 20:20:32 -0600

    list using loops

---
+++ 51_list_using_whileloops.py
@@ -0,0 +1,6 @@
+#Write a program to print the content of a list using while loops.
+l= [1, "Abir","Harry",False,"This","Rohan"]
+i=0
+while(i<len(l)):#length =7 and its condition is it will print until i is less than the 7 
+   print(l[i])
+   i +=1
