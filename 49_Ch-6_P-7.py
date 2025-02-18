78cc373d5bfca290397b711b26ff86488067022e
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-17 19:04:05 -0600

    p-7

---
+++ 49_Ch-6_P-7.py
@@ -0,0 +1,5 @@
+post=input("Enter the post:")
+if("Abir".upper() in post.upper()):#Turns the string to uppercase which identfieis if both are same or not
+   print("The post is talking about Abir")
+else:
+   print("The post is not talking about Abir")
