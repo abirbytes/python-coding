11753b4ecfe33909df08fc5b4bf1dc50fdb6a890
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-18 21:15:44 -0600

    p-2

---
+++ 70_Ch-8_P-2.py
@@ -0,0 +1,20 @@
+#Main code>>>
+def temperature(c):
+      return ((c*1.8)+32)
+c = float(input("Enter the temperature in celcius:"))
+cc= temperature(c)
+print(f"{round(cc,2)}")
+#Code no.2>>>
+# c = float(input("Enter the temperature in celcius:"))
+# def temperature():
+#    print((c*1.8)+32)
+# temperature()
+
+#Code no.3>>>
+# def temperature(c):
+#    if(c==0):
+#       return 32
+#    else:
+#       return ((c*1.8)+32)
+# a = temperature(2)
+# print(a)
