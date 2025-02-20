26e595280d3259d3e675c157aee516d7c7a15066
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-19 23:49:25 -0600

    edit

--- 59_ch-7p-6.py
+++ 59_ch-7p-6.py
@@ -1,6 +1,14 @@
-n=int(input("Enter the number:"))
+n=3
 product = 1
 for i in range(1,n+1):
-   product= product*i
-   print(f"The factorial of {n} is {product}")
-   
+   product*=i
+print(f"The factorial of {n} is {product}")
+
+#While loop
+x=5
+fact=1
+i=1
+while i<=x:
+   fact *=i
+   i+=1
+print(fact)
